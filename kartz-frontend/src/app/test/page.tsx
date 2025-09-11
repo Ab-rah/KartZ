7d52df515834b702003239b44906e0d80acd6417
abdhulrahimsheikh.m <mbabdhulrahim@gmail.com>
2025-09-11 12:13:42 +0530

    "Removed FrontEnd"

--- kartz-frontend/src/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useCart } from "@/contexts/CartContext";
-
-export default function TestPage() {
-  const { cart } = useCart();
-  return <div>Cart length: {cart.length}</div>;
-}
