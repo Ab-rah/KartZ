b772001bfbd89623996eedbb8d867db02d96bb16
abdhulrahimsheikh.m <mbabdhulrahim@gmail.com>
2025-09-11 10:42:09 +0530

    "Minor Tweak"

--- kartz-frontend/src/components/Navbar.tsx
+++ kartz-frontend/src/components/Navbar.tsx
@@ -61,7 +61,7 @@ export const NavbarClient = () => {
                     href="/seller/add-product"
                     className="px-4 py-2 rounded-lg text-gray-600 hover:text-gray-900 hover:bg-gray-100 transition-all duration-200"
                   >
-                    Manage Products
+                    Seller Dashboard
                   </Link>
                 )}
 
